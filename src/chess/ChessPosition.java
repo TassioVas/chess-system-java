3eba41d9829419c345ed4b68f3f0f7c625da1da8
Tassio Vasconcelos <t.santos.vasconcelos@gmail.com>
2020-07-02 16:11:11 -0300

    Completo Finalizado

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,13 +6,15 @@ public class ChessPosition {
 	
 	private char column;
 	private int row;
+	
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column > 'h' || row < 1 || row > 8 ) {
-			throw new ChessException("Erro na posição da peça. valores validos a1 ate h8");
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
 	}
+
 	public char getColumn() {
 		return column;
 	}
@@ -28,9 +30,9 @@ public class ChessPosition {
 	protected static ChessPosition fromPosition(Position position) {
 		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
-
+	
 	@Override
-      public String toString() {
+	public String toString() {
 		return "" + column + row;
 	}
 }
